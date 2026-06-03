d8ac00b4e002406b13066fe9c9bb8d6d827259e0
ocavue <ocavue@gmail.com>
2026-06-03 16:25:28 +1000

    refactor: load css in html

--- website/main.ts
+++ website/main.ts
@@ -1,5 +1,3 @@
-import './style.css'
-
 import { defineBasicExtension } from '@prosekit/basic'
 import { createEditor, union } from '@prosekit/core'
 
@@ -64,6 +62,7 @@ function start() {
 try {
   start()
 } catch (error) {
-  editorElement.textContent = 'Failed to start the editor. See console for details.'
+  editorElement.textContent =
+    'Failed to start the editor. See console for details.'
   throw error
 }
